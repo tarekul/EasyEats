8e26f9f528ad0432450adc30be7a6352e58fd6ee
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-04-27 19:32:38 -0400

    states added

--- src/containers/InfoForm.js
+++ src/containers/InfoForm.js
@@ -3,7 +3,14 @@ import '../components/infoform.css'
 import logo from '../components/logo.jpg';
 
 class InfoForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            states:['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+        }
+    }
     render(){
+        const {states}=this.state
         return (
             <>
             <form>
@@ -28,7 +35,7 @@ class InfoForm extends React.Component{
                 <label for="inputState">State</label>
                 <select id="inputState" class="form-control">
                     <option selected>Choose...</option>
-                    <option>...</option>
+                    <option>{states}</option>
                 </select>
                 </div>
                 <div className="form-group col-md-2">
