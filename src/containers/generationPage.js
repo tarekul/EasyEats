38533450a4a4296811edb105ec769d9ec10b4137
Tarek <bayridge4th@gmail.com>
2019-04-27 16:04:18 -0400

    added hashrouter and routes

---
+++ src/containers/generationPage.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class GenerationPage extends Component{
+    render(){
+        return <h1>GenerationPage</h1>
+    }
+}
+
+export default GenerationPage 
---
+++ src/containers/pollPage.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class PollPage extends Component{
+    render(){
+        return <h1>PollPage</h1>
+    }
+}
+
+export default PollPage 
