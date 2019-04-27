3909c467596ac3821e3f2a89a717b9fca30d0713
Ivy Strickland <IvyStrickland@pursuit.org>
2019-04-27 18:34:14 -0400

    update

--- src/containers/generationPage.js
+++ src/containers/generationPage.js
@@ -6,4 +6,66 @@ class GenerationPage extends Component{
     }
 }
 
+import React, { Component } from 'react';
+import List from '../components/restList';
+import axios from 'axios';
+
+class GenerationPage extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            restaurant: [
+                {
+                    name: '',
+                    img_url: '',
+                    cuisine: [],
+                },
+                {
+                    name: '',
+                    img_url: '',
+                    cuisine: [],
+                },
+                {
+                    name: '',
+                    img_url: '',
+                    cuisine: [],
+                }
+            ]
+
+        }
+    }
+
+
+    getOptions = (query) => {
+        axios({
+            method: 'get',
+            url: 'https://api.yelp.com/v3/businesses/search',
+            params: {
+                maxResults: 4,
+                key: '7qhXzmc-qBs_nON-yV8qSFRDQOJkB9e5UYMVuyik8ySqoilGOlVAvGE7F31YxftS2nEMUkugJUlS7PyM-D0nnUuaxq3BOKUVH0aHZipZHx48RP-X31AVCYz1bX7EXHYx',
+                location: '',
+
+            }
+        })
+    }
+
+
+    render() {
+        const { restaurant } = this.state
+        return (
+
+            <>
+                <form>
+                    {
+                        restaurant.map((e, i) => {
+                            return <List img_url={e.img_url} restaurant={e.restaurant} cuisine={e.cuisine} />
+                        })
+                    }
+                </form>
+            </>
+        )
+    }
+}
+
 export default GenerationPage 
