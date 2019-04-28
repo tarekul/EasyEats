e0f0dd7ac527f043c86d6fa8ef0ff5144273c35e
Robert Abreu <robertabreu@pursuit.org>
2019-04-28 14:37:34 -0400

    updated restList to work with voting options, added firebase updates (#23)

--- src/components/restList.js
+++ src/components/restList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const List = (props) => {
-    const { categories, image_url, menu_url, name, price, } = props;
+    const { categories, image_url, menu_url, name, price, poll, isDisabled, handleOnClick, isChecked } = props;
     return (
         <>
-            <div className='row container' >
+            <div className='row container mx-0' >
                 <div className='col-sm-12 col-md-2'>
                     <img className='card-img' style={{ height: '100px', width: '100%', objectFit: 'cover' }} src={image_url} alt={name} />
                 </div>
@@ -18,9 +18,18 @@ const List = (props) => {
                     <li className="list-group-item">
                         <div className='row'>
                             <div className='col col-xs-10'>
-                                <span  className='col h6' style={{paddingLeft: '2px', paddingRight: '2px'}}> {categories.join(', ')} </span>
+                                <span className='col h6' style={{ paddingLeft: '2px', paddingRight: '2px' }}> {categories.join(', ')} </span>
                             </div>
-                            {(price)? <span className='col-sm-2'>{price}</span>: <span className='col-sm-2'>--</span>}
+                            {(price) ? <span className='col-sm-1'>{price}</span> : <span className='col-sm-1'>--</span>}
+                            {
+                                (poll) ?
+                                    <div className=" col-sm-1 form-check form-check-inline">
+                                        <input className="form-check-input" type="checkbox" id="inlineCheckbox1" onClick={handleOnClick('voted_on')} value={name} disabled={isDisabled} checked={isChecked}/>
+                                        <label className="form-check-label h6" htmlFor="inlineCheckbox1">Vote</label>
+                                    </div>
+                                    :
+                                    <></>
+                            }
                         </div>
                     </li>
                 </div>
