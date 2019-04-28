df72be5de1417321b3d3063cebbc89fbecb4e54d
Robert Abreu <robertabreu@pursuit.org>
2019-04-28 05:52:08 -0400

    pollpage renders restaurant list correctly

--- src/containers/pollPage.js
+++ src/containers/pollPage.js
@@ -1,23 +1,72 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import List from '../components/restList'
+import { Redirect } from 'react-router-dom'
+import firebase from '../services/firebase';
 
-class PollPage extends Component{
-    render(){
+
+class PollPage extends Component {
+
+    state = {
+        redirect: false,
+        restaurants: null,
+    }
+
+    checkData = (id) => {
+        const firebaseRef = firebase.database().ref('/polls/' + id);
+        firebaseRef.once('value')
+            .then(snapshot => {
+                return snapshot.val()
+            })
+            .then( ({ data }) => {
+                this.setState({
+                    restaurants: data,
+                });
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    componentDidMount() {
+        const { id } = this.props.match.params || null;
+        if (!id) {
+            this.setState({
+                redirect: true,
+            })
+        } else {
+            this.checkData(id);
+        }
+    }
+    render() {
+        const { redirect, restaurants } = this.state;
         return (
             <>
-            <form>
-                <div class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input" id="customCheck1"></input>
-                
                 {
-                    List.map((e, i) => {
-                        return <List img_url={e.img_url} restaurant={e.restaurant} cuisine={e.cuisine} />
-                    })
+                    (redirect) ? <Redirect to='/' />
+                        :
+                        (restaurants) ?
+                            <div className="custom-control custom-checkbox">
+                                <input type="checkbox" className="custom-control-input" id="customCheck1"></input>
+                                {
+                                    restaurants.map((e, i) => {
+                                        return (
+                                            <div className='my-1' key={i}>
+                                                <List {...e} />
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                            :
+                            <div className='row'>
+                                <div className="col-sm-12 spinner-border" role="status">
+                                    <span className="sr-only">Loading...</span>
+                                </div>
+                            </div>
+
                 }
-                </div>
-            </form>
-        </>
-            )
+            </>
+        )
     }
 }
 
