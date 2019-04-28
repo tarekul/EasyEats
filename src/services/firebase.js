4dc167d6646b8f81730e7012500d284176914c0b
Robert Abreu <robertabreu@pursuit.org>
2019-04-28 04:45:10 -0400

    added firebase to project

---
+++ src/services/firebase.js
@@ -0,0 +1,15 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+    
+    authDomain: "easyeats-a662b.firebaseapp.com",
+    databaseURL: "https://easyeats-a662b.firebaseio.com",
+    projectId: "easyeats-a662b",
+    storageBucket: "easyeats-a662b.appspot.com",
+    messagingSenderId: "38887412175"
+  };
+
+app.initializeApp(config);
+
+export default app;
