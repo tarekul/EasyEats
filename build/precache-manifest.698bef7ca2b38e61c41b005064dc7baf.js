afc5ab7ddc96080e27a1350ce7f47e4e538ac1aa
Tarek <bayridge4th@gmail.com>
2019-04-28 16:04:50 -0400

    added build folder

---
+++ build/precache-manifest.698bef7ca2b38e61c41b005064dc7baf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76bf310249fa603133b4aace434303c8",
+    "url": "/EasyEats/index.html"
+  },
+  {
+    "revision": "edda827692abd2f81811",
+    "url": "/EasyEats/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1a678516420d4426ef6a",
+    "url": "/EasyEats/static/css/main.ff8feb79.chunk.css"
+  },
+  {
+    "revision": "edda827692abd2f81811",
+    "url": "/EasyEats/static/js/2.ff453203.chunk.js"
+  },
+  {
+    "revision": "1a678516420d4426ef6a",
+    "url": "/EasyEats/static/js/main.758e9e38.chunk.js"
+  },
+  {
+    "revision": "053138374fdde5746e61",
+    "url": "/EasyEats/static/js/runtime~main.89bd9bab.js"
+  },
+  {
+    "revision": "adb37c85b032b19017785bf05ccab074",
+    "url": "/EasyEats/static/media/logo.adb37c85.jpg"
+  }
+]);
