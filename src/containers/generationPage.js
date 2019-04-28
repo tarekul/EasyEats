f62e81a91d1f56e7f91ce2e079ae3f0fd117b0ab
Robert Abreu <robertabreu@pursuit.org>
2019-04-28 02:48:39 -0400

    formated generationPage

--- src/containers/generationPage.js
+++ src/containers/generationPage.js
@@ -9,8 +9,8 @@ class GenerationPage extends Component {
         super(props)
 
         this.state = {
-            lat:null,
-            lon:null,
+            lat: null,
+            lon: null,
             restaurants: null,
             display: [],
             redirect: false,
@@ -22,39 +22,39 @@ class GenerationPage extends Component {
         axios({
             method: 'GET',
             url: `${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=${this.state.lat}&longitude=${this.state.lon}&limit=50`,
-            headers:{
-               Authorization: 'BEARER 7qhXzmc-qBs_nON-yV8qSFRDQOJkB9e5UYMVuyik8ySqoilGOlVAvGE7F31YxftS2nEMUkugJUlS7PyM-D0nnUuaxq3BOKUVH0aHZipZHx48RP-X31AVCYz1bX7EXHYx'
-           }
+            headers: {
+                Authorization: 'BEARER 7qhXzmc-qBs_nON-yV8qSFRDQOJkB9e5UYMVuyik8ySqoilGOlVAvGE7F31YxftS2nEMUkugJUlS7PyM-D0nnUuaxq3BOKUVH0aHZipZHx48RP-X31AVCYz1bX7EXHYx'
+            }
         })
-        .then(res =>  parseYelpData(res.data))
-        .then( restaurants => this.setState( { restaurants }, () => {
-            localStorage.setItem('ee_restList', JSON.stringify(restaurants));
-            this.generateRandomRestaurantList();
-        }));
+            .then(res => parseYelpData(res.data))
+            .then(restaurants => this.setState({ restaurants }, () => {
+                localStorage.setItem('ee_restList', JSON.stringify(restaurants));
+                this.generateRandomRestaurantList();
+            }));
     }
     getOptions2 = (address) => {
         axios({
             method: 'GET',
             url: `${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?term=restaurants&location=${address}&limit=50`,
-            headers:{
-               Authorization: 'BEARER 7qhXzmc-qBs_nON-yV8qSFRDQOJkB9e5UYMVuyik8ySqoilGOlVAvGE7F31YxftS2nEMUkugJUlS7PyM-D0nnUuaxq3BOKUVH0aHZipZHx48RP-X31AVCYz1bX7EXHYx'
-           }
+            headers: {
+                Authorization: 'BEARER 7qhXzmc-qBs_nON-yV8qSFRDQOJkB9e5UYMVuyik8ySqoilGOlVAvGE7F31YxftS2nEMUkugJUlS7PyM-D0nnUuaxq3BOKUVH0aHZipZHx48RP-X31AVCYz1bX7EXHYx'
+            }
         })
-        .then(res =>  parseYelpData(res.data))
-        .then( restaurants => this.setState( { restaurants }, () => {
-            localStorage.setItem('ee_restList', JSON.stringify(restaurants));
-            this.generateRandomRestaurantList();
-        }));
+            .then(res => parseYelpData(res.data))
+            .then(restaurants => this.setState({ restaurants }, () => {
+                localStorage.setItem('ee_restList', JSON.stringify(restaurants));
+                this.generateRandomRestaurantList();
+            }));
     }
 
     generateRandomRestaurantList = (passedIn = null) => {
         const restaurants = passedIn || this.state.restaurants;
 
         if (!restaurants) {
-            this.setState({redirect: true})
+            this.setState({ redirect: true })
             return;
         };
-        const randomRestaurants = selectRandom(4 ,restaurants);
+        const randomRestaurants = selectRandom(4, restaurants);
         if (passedIn) {
             this.setState({
                 restaurants: passedIn,
@@ -76,38 +76,38 @@ class GenerationPage extends Component {
         const values = queryString.parse(this.props.location.search)
         if (!values.lat || !values.lon) {
             const temp = this.props.location.search.split('=')[1];
-            const ls = (localStorage.getItem('ee_loc'))? JSON.parse(localStorage.getItem('ee_loc')): null;
+            const ls = (localStorage.getItem('ee_loc')) ? JSON.parse(localStorage.getItem('ee_loc')) : null;
             if (ls) {
-                if(ls === temp) return this.pageReloaded();
+                if (ls === temp) return this.pageReloaded();
             }
-            const store = {loc: temp};
+            const store = { loc: temp };
             localStorage.setItem('ee_loc', JSON.stringify(store));
             this.getOptions2(temp);
         } else if (values.lat && values.lon) {
-            const ls = (localStorage.getItem('ee_latlon'))? JSON.parse(localStorage.getItem('ee_latlon')): null;
+            const ls = (localStorage.getItem('ee_latlon')) ? JSON.parse(localStorage.getItem('ee_latlon')) : null;
             if (ls) {
                 if (values.lat === ls.lat && values.lon === ls.lon) return this.pageReloaded();
             }
-            const store = {lat:values.lat, lon: values.lon};
+            const store = { lat: values.lat, lon: values.lon };
             localStorage.setItem('ee_latlon', JSON.stringify(store));
-            this.setState({lat: values.lat, lon: values.lon},()=>{
-                this.getOptions()   
-               })
+            this.setState({ lat: values.lat, lon: values.lon }, () => {
+                this.getOptions()
+            })
         } else {
-            this.setState({redirect: true})
+            this.setState({ redirect: true })
             return;
         }
     }
-    
+
     pageReloaded() {
-        const restaurants = (localStorage.getItem('ee_restList'))? JSON.parse(localStorage.getItem('ee_restList')): null;
+        const restaurants = (localStorage.getItem('ee_restList')) ? JSON.parse(localStorage.getItem('ee_restList')) : null;
         if (!restaurants) {
             localStorage.setItem('ee_loc', JSON.stringify(null));
             localStorage.setItem('ee_latlon', JSON.stringify(null));
             this.pageLoad();
             return;
         }
-        const display = (localStorage.getItem('ee_restDisplayList'))? JSON.parse(localStorage.getItem('ee_restDisplayList')): null;
+        const display = (localStorage.getItem('ee_restDisplayList')) ? JSON.parse(localStorage.getItem('ee_restDisplayList')) : null;
         if (!display) {
             this.generateRandomRestaurantList(restaurants);
             return;
@@ -118,7 +118,7 @@ class GenerationPage extends Component {
         });
     }
 
-    componentDidMount(){
+    componentDidMount() {
         // const {lat , lon} = this.state
         this.pageLoad();
     }
@@ -136,15 +136,15 @@ class GenerationPage extends Component {
                         })
                     }
                 </form>
-                <div className='container' style={{marginTop: '40px', marginLeft:'50px'}}  >
-                  <div>
-                <button type='button' class="btn btn-outline-info" >Generate New List</button>
+                <div className='container' style={{ marginTop: '40px', marginLeft: '50px' }}  >
+                    <div>
+                        <button type='button' class="btn btn-outline-info" >Generate New List</button>
+                    </div>
+                    <div style={{ marginTop: '20px' }}  >
+                        <button type='button' class="btn btn-outline-info" >Create Poll</button>
+                    </div>
                 </div>
-                <div style={{marginTop: '20px'}}  >
-                 <button type='button' class="btn btn-outline-info" >Create Poll</button>
-                </div>   
-                </div>
-               
+
             </>
         )
     }
