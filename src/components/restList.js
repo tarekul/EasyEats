01e430d47af70f9639e378818e6edb581348552e
Robert Abreu <robertabreu@pursuit.org>
2019-04-28 03:55:58 -0400

    modified styling; added pricing to restList

--- src/components/restList.js
+++ src/components/restList.js
@@ -1,16 +1,28 @@
 import React from 'react';
 
 const List = (props) => {
-    const { categories, display_phone, image_url, menu_url, name, phone, price,} = props;
+    const { categories, display_phone, image_url, menu_url, name, phone, price, } = props;
     return (
         <>
             <div className='row container' >
                 <div className='col-sm-12 col-md-2'>
-                    <img className='card-img' style={{ height: '100px', width: '100%', objectFit: 'cover'}} src={image_url} alt={name} />
+                    <img className='card-img' style={{ height: '100px', width: '100%', objectFit: 'cover' }} src={image_url} alt={name} />
                 </div>
                 <div className='card-header col' >
-                    <li className="list-group-item">{name}</li>
-                    <li className="list-group-item">{categories.map((e, i) => { return <span key={i}> {e} </span>})} <a href={menu_url} >Menu</a></li>
+                    <li className="list-group-item">
+                        <div className='row justify-content-between'>
+                            <span className='col h4'>{name}</span>
+                            <a href={menu_url} target='_blank' className='col-sm-2'>Menu</a>
+                        </div>
+                    </li>
+                    <li className="list-group-item">
+                        <div className='row'>
+                            <div className='col col-sm-10'>
+                                {categories.map((e, i) => { return <span key={i} className='col h6'> {e} </span> })}
+                            </div>
+                            <span className='col col-sm-2'>{price}</span>
+                        </div>
+                    </li>
                 </div>
             </div>
 
