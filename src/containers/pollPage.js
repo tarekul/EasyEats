9c1e285d44031b07c068c5a5e65f4460b4b7bbaa
Robert Abreu <robertabreu@pursuit.org>
2019-04-28 10:44:26 -0400

    added poll/check firebase; ready for PR

--- src/containers/pollPage.js
+++ src/containers/pollPage.js
@@ -7,8 +7,12 @@ import firebase from '../services/firebase';
 class PollPage extends Component {
 
     state = {
+        id: null,
         redirect: false,
         restaurants: null,
+        req_votes: null,
+        total_votes: null,
+        copySuccess: '',
     }
 
     checkData = (id) => {
@@ -24,7 +28,10 @@ class PollPage extends Component {
                     });
                 } else {
                     this.setState({
+                        id,
                         restaurants: data.data,
+                        req_votes: data.req_votes,
+                        total_votes: data.total_votes,
                     });
                 }
 
@@ -33,6 +40,15 @@ class PollPage extends Component {
                 console.log(err)
             })
     }
+
+    copyToClipboard = (e) => {
+        this.textArea.select();
+        document.execCommand('copy');
+        // This is just personal preference.
+        // I prefer to not show the the whole text area selected.
+        e.target.focus();
+        this.setState({ copySuccess: 'Copied!' });
+    };
 
     componentDidMount() {
         const { id } = this.props.match.params || null;
@@ -44,25 +60,63 @@ class PollPage extends Component {
             this.checkData(id);
         }
     }
+
+    componentDidUpdate(pp, ps) {
+        console.log(this.state)
+    }
     render() {
-        const { redirect, restaurants } = this.state;
+        const { id, redirect, restaurants, req_votes } = this.state;
         return (
             <>
                 {
                     (redirect) ? <Redirect to='/' />
                         :
                         (restaurants) ?
-                            <div className="custom-control custom-checkbox">
-                                <input type="checkbox" className="custom-control-input" id="customCheck1"></input>
-                                {
-                                    restaurants.map((e, i) => {
-                                        return (
-                                            <div className='my-1' key={i}>
-                                                <List {...e} />
+                            <div className="">
+                                <div className='row'>
+                                    <div className='col-sm-12 col-md-6 row'>
+                                        {/* <input className="form-control" type="text"  value={'http://localhost:3000/#/poll/'+id} disabled/> */}
+                                        {/* <span className="input-group-text col-sm-8" id="basic-addon3">{'http://localhost:3000/#/poll/' + id}</span> */}
+                                        <form className='col col-sm-8'
+                                        style={{height: '0px', width: '0px', backgroundColor: 'grey'}}>
+                                            <textarea
+                                                style={{height: '0px', width: '0px', backgroundColor: 'grey'}}
+                                                ref={(textarea) => this.textArea = textarea}
+                                                value={'http://localhost:3000/#/poll/' + id}
+                                                readOnly={true}
+                                            />
+                                        </form>
+                                        {
+                                            /* Logical shortcut for only displaying the 
+                                               button if the copy command exists */
+                                            document.queryCommandSupported('copy') &&
+                                            <div className='col-sm-4'>
+                                                <button className='btn btn-outline-info col-sm-12' onClick={this.copyToClipboard}>Copy Url</button>
+                                                {this.state.copySuccess}
                                             </div>
-                                        )
-                                    })
-                                }
+                                        }
+                                    </div>
+                                </div>
+                                <div className='row'>
+                                    <div className='col col-11 mr-0 pr-0'>
+                                        {
+                                            restaurants.map((e, i) => {
+                                                return (
+                                                    <div className='my-1' key={i}>
+                                                        <List {...e} />
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                    <div className='col col-1 my-1 mx-0 p-2'>
+                                        {
+                                            [1, 2, 3, 4].map((e, i) => {
+                                                return <div style={{ backgroundColor: 'red', height: '10px' }} key={i}></div>
+                                            })
+                                        }
+                                    </div>
+                                </div>
                             </div>
                             :
                             <div className="d-flex justify-content-center mt-5 pt-5"><div className="spinner-border" role="status"><span className="sr-only">Loading...</span></div></div>
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,7 +2,7 @@ import app from 'firebase/app';
 import 'firebase/database';
 
 const config = {
-  
+  apiKey: "AIzaSyB6IU_v1uW_H1xso-2pP5EfcW1MCwQhOfA",
   authDomain: "easyeats-a662b.firebaseapp.com",
   databaseURL: "https://easyeats-a662b.firebaseio.com",
   projectId: "easyeats-a662b",
