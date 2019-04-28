5a2e95fa1616cb26e50c890ee571b65878e9d604
Robert Abreu <robertabreu@pursuit.org>
2019-04-28 02:55:17 -0400

    restList updated to function with new restaurant object

--- src/components/restList.js
+++ src/components/restList.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
 const List = (props) => {
+    const { categories, display_phone, image_url, menu_url, name, phone, price,} = props;
     return (
         <>
             <div className='row container' >
                 <div className='col-2'>
-                    <img className='card-img' style={{ width: '100px' }} src={props.img_url} alt='../assets/logo.png' />
+                    <img className='card-img' style={{ width: '100px' }} src={image_url} alt={name} />
                 </div>
                 <div className='card-header col' >
-                    <li className="list-group-item">Name of Restaurant {props.restaurant}</li>
-                    <li className="list-group-item">Type of Restaurant {props.cuisine} <a href='' src={props.menu} >Menu</a></li>
+                    <li className="list-group-item">{name}</li>
+                    <li className="list-group-item">{categories.map((e, i) => { return <span key={i}> {e} </span>})} <a href={menu_url} >Menu</a></li>
                 </div>
             </div>
 
