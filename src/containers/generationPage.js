f7bb0691cd27b2512e886398efe82245a0a04c0d
Robert Abreu <robertabreu@pursuit.org>
2019-04-28 02:47:27 -0400

    implemented localstorage usage for generationPage container

--- src/containers/generationPage.js
+++ src/containers/generationPage.js
@@ -28,6 +28,7 @@ class GenerationPage extends Component {
         })
         .then(res =>  parseYelpData(res.data))
         .then( restaurants => this.setState( { restaurants }, () => {
+            localStorage.setItem('ee_restList', JSON.stringify(restaurants));
             this.generateRandomRestaurantList();
         }));
     }
@@ -41,37 +42,54 @@ class GenerationPage extends Component {
         })
         .then(res =>  parseYelpData(res.data))
         .then( restaurants => this.setState( { restaurants }, () => {
+            localStorage.setItem('ee_restList', JSON.stringify(restaurants));
             this.generateRandomRestaurantList();
         }));
     }
 
-    generateRandomRestaurantList = () => {
-        const { restaurants } = this.state;
-        console.log('generating random: ', restaurants)
+    generateRandomRestaurantList = (passedIn = null) => {
+        const restaurants = passedIn || this.state.restaurants;
+
         if (!restaurants) {
             this.setState({redirect: true})
             return;
         };
         const randomRestaurants = selectRandom(4 ,restaurants);
-        this.setState({
-            display: randomRestaurants,
-        })
+        if (passedIn) {
+            this.setState({
+                restaurants: passedIn,
+                display: randomRestaurants,
+            }, () => {
+                localStorage.setItem('ee_restDisplayList', JSON.stringify(randomRestaurants));
+            })
+        } else {
+            this.setState({
+                display: randomRestaurants,
+            }, () => {
+                localStorage.setItem('ee_restDisplayList', JSON.stringify(randomRestaurants));
+            })
+        }
     }
 
-    componentDidMount(){
-        // const {lat , lon} = this.state
+    pageLoad() {
 
         const values = queryString.parse(this.props.location.search)
-        console.log('location',this.props.location.search)
-        console.log('lat',values.lat)
-        console.log('lon',values.lon)
         if (!values.lat || !values.lon) {
             const temp = this.props.location.search.split('=')[1];
-            // const address = (temp.includes('%20'))? temp.replace(/%20/g, " "): temp;
-            // console.log('address: ', address)
+            const ls = (localStorage.getItem('ee_loc'))? JSON.parse(localStorage.getItem('ee_loc')): null;
+            if (ls) {
+                if(ls === temp) return this.pageReloaded();
+            }
+            const store = {loc: temp};
+            localStorage.setItem('ee_loc', JSON.stringify(store));
             this.getOptions2(temp);
         } else if (values.lat && values.lon) {
-            
+            const ls = (localStorage.getItem('ee_latlon'))? JSON.parse(localStorage.getItem('ee_latlon')): null;
+            if (ls) {
+                if (values.lat === ls.lat && values.lon === ls.lon) return this.pageReloaded();
+            }
+            const store = {lat:values.lat, lon: values.lon};
+            localStorage.setItem('ee_latlon', JSON.stringify(store));
             this.setState({lat: values.lat, lon: values.lon},()=>{
                 this.getOptions()   
                })
@@ -79,6 +97,30 @@ class GenerationPage extends Component {
             this.setState({redirect: true})
             return;
         }
+    }
+    
+    pageReloaded() {
+        const restaurants = (localStorage.getItem('ee_restList'))? JSON.parse(localStorage.getItem('ee_restList')): null;
+        if (!restaurants) {
+            localStorage.setItem('ee_loc', JSON.stringify(null));
+            localStorage.setItem('ee_latlon', JSON.stringify(null));
+            this.pageLoad();
+            return;
+        }
+        const display = (localStorage.getItem('ee_restDisplayList'))? JSON.parse(localStorage.getItem('ee_restDisplayList')): null;
+        if (!display) {
+            this.generateRandomRestaurantList(restaurants);
+            return;
+        }
+        this.setState({
+            restaurants,
+            display,
+        });
+    }
+
+    componentDidMount(){
+        // const {lat , lon} = this.state
+        this.pageLoad();
     }
 
     render() {
