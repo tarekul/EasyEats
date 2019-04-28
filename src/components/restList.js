0e3f5c4b939a2c62decc147e662f76ef00d8bb86
Robert Abreu <robertabreu@pursuit.org>
2019-04-28 02:49:52 -0400

    formated restList component

--- src/components/restList.js
+++ src/components/restList.js
@@ -3,15 +3,15 @@ import React from 'react';
 const List = (props) => {
     return (
         <>
-        <div className='row container' >
-        <div className='col-2'>
-             <img className='card-img' style={{width:'100px'}} src={props.img_url} alt='../assets/logo.png' />
-        </div>
-        <div className='card-header col' >
-        <li className="list-group-item">Name of Restaurant {props.restaurant}</li>
-     <li className="list-group-item">Type of Restaurant {props.cuisine} <a href='' src={props.menu} >Menu</a></li>
-        </div>            
-        </div>
+            <div className='row container' >
+                <div className='col-2'>
+                    <img className='card-img' style={{ width: '100px' }} src={props.img_url} alt='../assets/logo.png' />
+                </div>
+                <div className='card-header col' >
+                    <li className="list-group-item">Name of Restaurant {props.restaurant}</li>
+                    <li className="list-group-item">Type of Restaurant {props.cuisine} <a href='' src={props.menu} >Menu</a></li>
+                </div>
+            </div>
 
 
         </>
