a5982e5f3af9e1b3b0954ebb3de52fbb3345c992
Robert Abreu <robertabreu@pursuit.org>
2019-04-28 05:33:21 -0400

    fixed styling

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/database';
 
 const config = {
   
-    authDomain: "easyeats-a662b.firebaseapp.com",
-    databaseURL: "https://easyeats-a662b.firebaseio.com",
-    projectId: "easyeats-a662b",
-    storageBucket: "easyeats-a662b.appspot.com",
-    messagingSenderId: "38887412175"
-  };
+  authDomain: "easyeats-a662b.firebaseapp.com",
+  databaseURL: "https://easyeats-a662b.firebaseio.com",
+  projectId: "easyeats-a662b",
+  storageBucket: "easyeats-a662b.appspot.com",
+  messagingSenderId: "38887412175"
+};
 
 app.initializeApp(config);
 
