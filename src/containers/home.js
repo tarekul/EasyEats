8e852bfbc9886b47b0ecb70ba4df3ebf2513d1d5
Tarek Islam <bayridge4th@gmail.com>
2019-05-05 11:32:44 -0400

    Merging dev-master 1.0.0 to master branch (#52)
    * All changes (#1)
    
    *  logo image add to the infos page and components folder
    
    *  info page display done, need to store adress for API calls
    
    * page done
    
    * Made homepage and button component
    
    * update
    
    * got lat and lon
    
    * started axios call
    
    * states added
    
    *  added buttons to generation page
    
    *  got res data
    
    *  state form working
    
    * saved form data and sent thru params
    
    * created service file to parse yelp data
    
    * added randomizer functions in service
    
    * implemented random restaurant list generation when page loads for first time
    
    * implemented localstorage usage for generationPage container
    
    * formated generationPage
    
    * formated restList component
    
    * restList updated to function with new restaurant object
    
    * spread display array elements into List component
    
    * added object fit cover property to images in restList component
    
    * cleaned up render return, added handleClick for generate new list
    
    * modified styling; added pricing to restList
    
    * wrapped List components in div for styling
    
    * removed categories map and replaced with join
    
    * added firebase to project
    
    * onClick added to generate poll button, will generate random id, firebase to be tested
    
    * write to firebase successfull, when finished moved page to pollPage
    
    * firebase config now uses correct module
    
    * fixed styling
    
    * fixed warnings for resList
    
    * removed firebase console.log
    
    * pollpage renders restaurant list correctly
    
    * if id does not match a list in the db, users are redirected to homepage
    
    * added loading spinners
    
    * removed console log from generationPage
    
    * added spinner while component is loading cords
    
    * installed jQuery and popperJS for modal functionality
    
    * added a new modal component when generate poll is click, asks for num of votes required
    
    * changed button language
    
    * added poll/check firebase; ready for PR
    
    * missing break statement (#15)
    
    * Tarek7 (#16)
    
    * missing break statement
    
    * adding build for gh-pages deploy
    
    * added logo to homepage (#17)
    
    * added alt tom img tag and deleted duplicate props
    
    *  favicon
    
    * added features to home page
    
    *  favicon added and working
    
    * centered logo (#22)
    
    * updated restList to work with voting options, added firebase updates (#23)
    
    * info form
    
    * new build (#24)
    
    * added to package (#25)
    
    * completed info form
    
    * variable to tally totalVotes created
    
    * changed deploy
    
    * edited package file
    
    * moved code to info component
    
    *  styling finsihed
    
    *  styling changing done
    
    * styling for the generator page
    
    * added build folder
    
    * added build folder (#29)
    
    * implemented realtime database
    
    * cleaned up some console logs and alt tag error
    
    * added unique restaurants edited services
    
    * semifinal update
    
    * final commit
    
    * added logo to poll page (#33)
    
    * Update README.md (#34)
    
    * fixed URL
    
    * added hash
    
    * fixed link again sorry
    
    * implemented getRestaurantsByAddress calling backend
    
    * implemented getRestaurantsByGeo which calls api backend
    
    * fixed spinner for page load on home container, cleaned up code
    
    * added origins to headers for api calls
    
    * secured firebase backend
    
    * readded token
    
    * firebasejs now configures correctly
    
    * had to setTimeouts on pollpage, asyncronous nature of firebase.js causes app to crash on page refresh
    
    * deleted whitespace, also added componentWillUnmount in previous commit

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,75 +1,63 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Button from '../components/Button'
 import Sky from 'react-sky';
 import logo from '../components/logo.jpg';
 
-class Home extends Component{
+class Home extends Component {
     state = {
         loading: null,
     }
 
-    geo = () =>{
+    geo = () => {
         this.setState({
             loading: true,
-        }, () => {
-            setTimeout( () => {
-                this.setState({
-                    loading: false,
-                })
-            }, 1500);
         });
-       if(navigator.geolocation) {
+        if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
                 (position) => {
-                    const {latitude,longitude} = position.coords
-                    console.log('latitude:' + latitude, 'Longitude:' + longitude)
-                    // this.setState({latitude:position.coords.latitude,longitude:position.coords.longitude},()=>{
-                    //     console.log(this.state)
-                    // })
-                    this.props.history.push(`/genpoll?lat=${latitude}&lon=${longitude}`)
+                    const { latitude, longitude } = position.coords;
+                    this.props.history.push(`/genpoll?lat=${latitude}&lon=${longitude}`);
                 })
         }
     }
 
-    // componentDidUpdate(pp, ps) {
-    //     console.log('previous state: ', ps)
-    //     console.log('current state: ', this.state);
-    // }
+    render() {
+        const { loading } = this.state;
+        return (
+            <>
+                <Sky
+                    images={{
 
-    render(){
-        return <> 
-        <Sky 
-          images={{
-      
-              0: "https://image.flaticon.com/icons/svg/706/706195.svg",
-                1: "https://image.flaticon.com/icons/svg/1676/1676981.svg",
-                2: "https://image.flaticon.com/icons/svg/1591/1591724.svg",
-                3: "https://image.flaticon.com/icons/svg/273/273804.svg",
-                4: "https://image.flaticon.com/icons/svg/1672/1672272.svg",
+                        0: "https://image.flaticon.com/icons/svg/706/706195.svg",
+                        1: "https://image.flaticon.com/icons/svg/1676/1676981.svg",
+                        2: "https://image.flaticon.com/icons/svg/1591/1591724.svg",
+                        3: "https://image.flaticon.com/icons/svg/273/273804.svg",
+                        4: "https://image.flaticon.com/icons/svg/1672/1672272.svg",
 
-                
-                
-          }}
-          how={130} 
-          time={25} 
-          size={'90px'}
-          background={'teal'}
-        />
 
-            <div className='container mt-5'>
-            <div className ='row justify-content-md-center'>
-            <div className='col-md-12 row justify-content-md-center'>
-                <img className='col col-md-auto' src={logo} alt='site logo'/>
-            </div>
-                {/* <h2 style={{textAlign:'center'}}>EasyEats</h2> */}
-                <div className='col-md-12 row justify-content-md-center m-2'>
-                    <span style={{marginRight:'1%'}}><Button className='col col-md-6'goTo='/' value='My Location' handleOnClick={e=>{this.geo()}}/></span>
-                    <Button className='col col-md-6' goTo='/infopage' value='Enter Location' />
+
+                    }}
+                    how={130}
+                    time={25}
+                    size={'90px'}
+                    background={'teal'}
+                />
+
+                <div className='container mt-5'>
+                    <div className='row justify-content-md-center'>
+                        <div className='col-md-12 row justify-content-md-center'>
+                            <img className='col col-md-auto' src={logo} alt='site logo' />
+                        </div>
+                        {/* <h2 style={{textAlign:'center'}}>EasyEats</h2> */}
+                        <div className='col-md-12 row justify-content-md-center m-2'>
+                            <span style={{ marginRight: '1%' }}><Button className='col col-md-6' goTo='/' value='My Location' handleOnClick={e => { this.geo() }} /></span>
+                            <Button className='col col-md-6' goTo='/infopage' value='Enter Location' />
+                        </div>
+                        {(loading) ? (<div className="d-flex justify-content-center mt-5 pt-5"><div className="spinner-border" role="status"><span className="sr-only">Loading...</span></div></div>) : <></>}
+                    </div>
                 </div>
-                {(!this.state) ? (<div className="d-flex justify-content-center mt-5 pt-5"><div className="spinner-border" role="status"><span className="sr-only">Loading...</span></div></div>): <></>}
-            </div>
-            </div>
-        </>
+            </>
+        )
     }
 }
 
